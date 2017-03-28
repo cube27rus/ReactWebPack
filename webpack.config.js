4c3138563cd2a58f5501e3fc00e0763117c7ba9c
cube27rus <nrpo27@yandex.ru>
2017-03-28 22:15:24 +1000

    настройка вебпака

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: './src/app.jsx',//основной фаил приложения
+    output:{//куда поместить фаил сборки
+        filename: 'bundle.js',
+        path: __dirname + "/public"//
+
+    } ,
+    //настройка сервера
+    devServer: {
+        inline:true, //не выводить строку сверху страницы
+        contentBase: './public', //корневая директория
+        port: 3000 //порт
+    },
+    module: {//загрузчики
+        loaders: [
+            {
+                test: /\.jsx?$/,//расширения фаилов пропускаемые через загрузчик
+                exclude: /node_modules/,//не трогаем эту папку
+                loader: 'babel-loader'//загрузчик
+            }
+        ]
+    },
+    //показывает дебаг в нужном месте
+    devtool: 'eval-source-map',
+    //расширения для загрузки компонентов
+    resolve: {
+        extensions: [" ",".js",".jsx"]
+    }
+};
